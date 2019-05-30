56149f7102d2f2f4dca0557813959118a94792d5
kandhavivekraj <kandhavivekraj@gmail.com>
2019-05-30 10:57:40 +0530

    Handle Route Exit

--- addon/components/toggle-component.js
+++ addon/components/toggle-component.js
@@ -1,17 +1,18 @@
 import Component from '@ember/component';
 import layout from '../templates/components/toggle-component';
 import { inject as service } from '@ember/service';
+import { oneWay } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
   tagName: 'span',
   classNames: ['access-toggle-button'],
-  classNameBindings: ['isToggleLoading:disabled', 'isToggleSelected:access-toggle-button-selected'],
   accessTestService: service('access-test'),
+  isToggleSelected: oneWay('accessTestService.canShowSpan'),
+  classNameBindings: ['isToggleSelected:access-toggle-button-selected'],
 
   click() {
     this.accessTestService.toggleAccess();
-    this.set('isToggleSelected', !this.get('isToggleSelected'));
   },
 
 });
--- app/initializers/ember-access-ext-init.js
+++ app/initializers/ember-access-ext-init.js
@@ -1,4 +1,4 @@
-// import ext from 'access-test-addon/ext/ember-access-ext'; // eslint-disable-line no-unused-vars
+import ext from 'access-test-addon/ext/ember-access-ext'; // eslint-disable-line no-unused-vars
 
 export function initialize() {
   // nothing else needs to be done here
