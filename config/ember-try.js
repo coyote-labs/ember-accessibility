6c8da4ec3c0635468a8c2129f7d7cb1961ca3e0e
Gaurav Munjal <Gaurav0@aol.com>
2022-07-25 11:10:57 -0400

    chore: [BREAKING] remove ember-angle-bracket-invocation-polyfill

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,22 +11,42 @@ module.exports = function() {
     return {
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
+          name: 'ember-lts-3.12',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
+              'ember-source': '~3.12.0'
             }
           }
         },
         {
-          name: 'ember-lts-3.4',
+          name: 'ember-lts-3.16',
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0'
+              'ember-source': '~3.16.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.20',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.20.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.24',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.24.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.28',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.28.0'
             }
           }
         },
